e8edc0b6b5bf50f4743dadd65e05ecfdd85e6938
Nate <natemkelley@gmail.com>
2017-11-17 03:27:35 +0000

    final swag

--- models/Pictures.js
+++ models/Pictures.js
@@ -3,15 +3,7 @@ var mongoose = require('mongoose');
 var pictureSchema = new mongoose.Schema({
     url: String,
     caption: String
-    // upvotes: {
-    //     type: Number,
-    //     default: 0
-    // },
 });
-//
-// CommentSchema.methods.upvote = function (cb) {
-//     this.upvotes += 1;
-//     this.save(cb);
-// };
+
 
 mongoose.model('Picture', pictureSchema);
